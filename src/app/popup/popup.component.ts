f2199b6392d5b4e1b279d7476ab4eb649c41d51c
Timo Schrappe <schrappe.t@thirdman.de>
2018-05-22 12:30:17 +0200

    Moved the popups outside of the toolbar

--- src/app/popup/popup.component.ts
+++ src/app/popup/popup.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 @Component({
   selector: 'app-popup',
   templateUrl: './popup.component.html',
-  styleUrls: ['./popup.component.css']
+  styleUrls: ['./popup.component.scss']
 })
 export class PopupComponent implements OnInit {
   @Output() close = new EventEmitter();
