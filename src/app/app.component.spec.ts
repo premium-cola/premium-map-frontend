0a006d4845d5741e2e03e6d98bbf3937d75a6240
Christoph Haefner <christoph.haefner@mailbox.org>
2021-04-01 17:31:41 +0200

    Update project name to match repo name: "premium-map-frontend"

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'map-frontend'`, () => {
+  it(`should have as title 'premium-map-frontend'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('map-frontend');
+    expect(app.title).toEqual('premium-map-frontend');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('map-frontend app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('premium-map-frontend app is running!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'map-frontend';
+  title = 'premium-map-frontend';
 }
